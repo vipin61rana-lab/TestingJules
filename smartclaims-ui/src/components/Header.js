f825e8618a72575c7f4c3b0a70671383a473b8a9
vipin61rana-lab <vipin61rana@gmail.com>
2025-09-16 15:37:33 +0000

    header fix

--- smartclaims-ui/src/components/Header.js
+++ smartclaims-ui/src/components/Header.js
@@ -12,7 +12,15 @@ const Header = () => {
         navigate('/login');
     };
 
-    const userRoles = user?.roles ? user.roles.split(',') : [];
+    // Ensure user.roles is a string before splitting
+    let userRoles = [];
+    if (user?.roles) {
+        if (Array.isArray(user.roles)) {
+            userRoles = user.roles;
+        } else if (typeof user.roles === 'string') {
+            userRoles = user.roles.split(',');
+        }
+    }
     const isAdmin = userRoles.includes('ROLE_ADMIN');
 
     return (
